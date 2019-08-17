de28ec9779acd79f9ca4369ff6552abc5ab9457b
TarcisioNeto98 <tarcisioneto1998@gmail.com>
2019-08-17 14:23:33 -0300

    Modificações no javascript e adicionado o framework jquery

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,20 @@
-var botao = document.getElementById("principal"), contador = 0, corpo = document.querySelector("body");
-	botao.onclick = function(){
-		var navegador = document.getElementById("nav");
-		if(contador % 2 == 0){
-			navegador.className = "navegador";
-			navegador.innerHTML = 
-			'<nav class="menu"><ul class="lista"><li><a href="#">Home</a></li><li><a href = "#">Projetos</a></li><li><a href="#">Contato</a></li></ul></nav>';
-			botao.textContent = " Voltar";
-			contador++;
-		}
-		else{
-			navegador.className = "";
-			contador++;
-			navegador.innerHTML = "";
-			botao.textContent = " Menu";
-		}
-
-		corpo.onclick = function(){
-			navegador.className = "";
-		};
-
-};
+var botao = $('#principal'), contador = 0, corpo = $('body');
+//botao.css({"border-bottom-width": "2px", "border-bottom-color": "#00FF7F"});
+var navegador = $('#nav');
+var nome = "neto";
+alert(nome.toUpperCase());
+botao.click(function(){
+	botao.toggleClass('fundo');
+	navegador.toggleClass("navegador");
+	if(contador % 2 == 0){
+		navegador.append('<nav class="menu"><ul class="lista"><li><a href="#">Home</a></li><li><a href = "#">Projetos</a></li><li><a href="#">Contato</a></li></ul></nav>');
+		botao.text(" Voltar");
+		contador++;
+	}
+	else{
+		alert("aaa");
+		$('nav[class = menu]').remove();
+		botao.text(" Menu");
+		contador++;
+	}
+});
